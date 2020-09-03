4f8d94d4ea4aae7e470b7fb767103611382f4dfa
b3m2a1 <b3m2a1@gmail.com>
2020-09-03 15:47:39 -0400

    Getting nbgitpuller installed

--- jupyter_config.py
+++ jupyter_config.py
@@ -1,3 +1,3 @@
-# Supposedly we can put all Jupyter config stuff here?
-# All I have is this recommendation from Binder...
-c.LabBuildApp.minimize = False
+# # Supposedly we can put all Jupyter config stuff here?
+# # All I have is this recommendation from Binder...
+# c.LabBuildApp.minimize = False
